a88567237991c28c54f0f394555668929fd6d36a
Jasen Moloy <jasenmoloy.dev@gmail.com>
2016-01-29 16:20:15 -0800

    Add custom action bar and "add" action button.

--- app/src/main/java/jasenmoloy/wirelesscontrol/MainActivity.java
+++ app/src/main/java/jasenmoloy/wirelesscontrol/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,18 +19,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Display a preference fragment as the main content for setting up
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
-
-        //Set default parameters for all preferences.
-        PreferenceManager.setDefaultValues(this, R.xml.settings_general, false);
-        PreferenceManager.setDefaultValues(this, R.xml.settings_wifi, false);
-        PreferenceManager.setDefaultValues(this, R.xml.settings_bluetooth, false);
-
         //Set the active view
         setContentView(R.layout.activity_main);
+
+        //Set the active toolbar
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+
+//        //Set default parameters for all preferences.
+//        PreferenceManager.setDefaultValues(this, R.xml.settings_general, false);
+//        PreferenceManager.setDefaultValues(this, R.xml.settings_wifi, false);
+//        PreferenceManager.setDefaultValues(this, R.xml.settings_bluetooth, false);
+//
+//        // Display a preference fragment as the main content for setting up
+//        getFragmentManager().beginTransaction()
+//                .replace(android.R.id.content, new SettingsFragment())
+//                .commit();
     }
 
     @Override
