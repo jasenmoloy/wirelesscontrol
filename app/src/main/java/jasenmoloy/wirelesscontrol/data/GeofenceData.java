019b1e6af1afa1108389f938a58af1aa5a485164
Jasen Moloy <jasenmoloy.dev@gmail.com>
2016-02-25 17:29:40 -0800

    Create containers for describing geofence data to be saved and what UI elements a geofence marker has.

---
+++ app/src/main/java/jasenmoloy/wirelesscontrol/data/GeofenceData.java
@@ -0,0 +1,12 @@
+package jasenmoloy.wirelesscontrol.data;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by jasenmoloy on 2/25/16.
+ */
+public class GeofenceData {
+    public String name;
+    public LatLng position;
+    public double radius;
+}
---
+++ app/src/main/java/jasenmoloy/wirelesscontrol/ui/GeofenceMarker.java
@@ -0,0 +1,12 @@
+package jasenmoloy.wirelesscontrol.ui;
+
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by jasenmoloy on 2/25/16.
+ */
+public class GeofenceMarker {
+    public Marker marker;
+    public Circle circle;
+}
