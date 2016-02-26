deba8cf2bb80eda8bb01d73fb6d1f7642b18b263
Jasen Moloy <jasenmoloy.dev@gmail.com>
2016-02-25 17:40:24 -0800

    Create a text "template" file for class instantiation. Will find a better solution in the future.
    Essentially it's a text file to reference (read: copy/paste) when building a new class to faster developing.

---
+++ app/src/main/java/jasenmoloy/wirelesscontrol/template/ClassTemplate.java
@@ -0,0 +1,30 @@
+package jasenmoloy.wirelesscontrol.template;
+
+/**
+ * Created by jasenmoloy on 2/25/16.
+ */
+public interface ClassTemplate {
+    /// ----------------------
+    /// Class Fields
+    /// ----------------------
+
+    /// ----------------------
+    /// Object Fields
+    /// ----------------------
+
+    /// ----------------------
+    /// Public Methods
+    /// ----------------------
+
+    /// ----------------------
+    /// Callback Methods
+    /// ----------------------
+
+    /// ----------------------
+    /// Protected Methods
+    /// ----------------------
+
+    /// ----------------------
+    /// Private Methods
+    /// ----------------------
+}
