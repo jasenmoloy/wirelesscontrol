2c14b31627e0bac071719051338f116ed0e0309c
Jasen Moloy <jasenmoloy.dev@gmail.com>
2016-02-25 17:34:11 -0800

    Create a Geofance save listener

---
+++ app/src/main/java/jasenmoloy/wirelesscontrol/io/OnGeofenceSaveFinishedListener.java
@@ -0,0 +1,10 @@
+package jasenmoloy.wirelesscontrol.io;
+
+/**
+ * Created by jasenmoloy on 2/25/16.
+ */
+public interface OnGeofenceSaveFinishedListener {
+    void onGeofenceSaveError();
+
+    void onGeofenceSaveSuccess();
+}
