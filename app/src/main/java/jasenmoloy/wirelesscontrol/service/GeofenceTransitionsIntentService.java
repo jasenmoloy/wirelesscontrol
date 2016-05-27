a975853389b489743708c43544b6b23e11378673
Jasen Moloy <jasenmoloy.dev@gmail.com>
2016-05-27 12:30:08 -0700

    GeofenceTransitionsIntentService: Import package cleanup

--- app/src/main/java/jasenmoloy/wirelesscontrol/service/GeofenceTransitionsIntentService.java
+++ app/src/main/java/jasenmoloy/wirelesscontrol/service/GeofenceTransitionsIntentService.java
@@ -1,27 +1,17 @@
 package jasenmoloy.wirelesscontrol.service;
 
 import android.app.IntentService;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
-import android.text.TextUtils;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import jasenmoloy.wirelesscontrol.R;
 import jasenmoloy.wirelesscontrol.debug.Debug;
 import jasenmoloy.wirelesscontrol.debug.ErrorMessages;
 import jasenmoloy.wirelesscontrol.managers.GeofenceTransitionManager;
-import jasenmoloy.wirelesscontrol.ui.MainActivity;
 
 /**
  * Created by jasenmoloy on 3/10/16.
